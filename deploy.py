3dbe3e8dc1df8e04d85eec5626c7aaeeca0088e0
ronpichardo <ron@ronpich.com>
2021-07-25 14:21:08 -0400

    Created deploy script, to push updated builds to the server

--- deploy.py
+++ deploy.py
@@ -1,14 +1,27 @@
 import requests
-import os, sys
+import os, sys, json
 
 vc4_server = os.getenv('VC4_SERVER_URL')
 vc4_api_key = os.getenv('VC4_API_KEY')
 
+file_path = sys.argv[1]
+
 headers = {
     'accept': 'application/json',
-    'Authorization': vc4_api_key
+    'Authorization': vc4_api_key,
+    'Content-Type': 'multipart/form-data'
 }
 
-deploy = requests.get(f'{vc4_server}/VirtualControl/config/api/ProgramLibrary', headers=headers)
+form_data = {
+    'ProgramId': 2,
+    'FriendlyName': 'SSProJenkins',
+    'Notes': 'Jenkins Deployed This',
+    'StartNow': True
+}
 
+program_file = {
+    'AppFile': open(file_path, 'rb')
+}
+
+deploy = requests.post(f'{vc4_server}/VirtualControl/config/api/ProgramLibrary', headers=headers, files=program_file, data=form_data)
 print(deploy.content)
